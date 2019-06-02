2f82729b94e0bf8fadeb65aeb1ab96c0e6eb26e5
Tyler Ward <tylerjward@gmail.com>
2019-06-01 21:23:51 -0500

    turned all red into green:)

--- src/test/java/com/pairgood/AboutArithmeticOperators.java
+++ src/test/java/com/pairgood/AboutArithmeticOperators.java
@@ -8,49 +8,49 @@ import static org.junit.Assert.assertEquals;
 public class AboutArithmeticOperators {
     @Test
     public void simpleOperations() {
-        assertEquals(1, __);
-        assertEquals(1 + 1, __);
-        assertEquals(2 + 3 * 4, __);
-        assertEquals((2 + 3) * 4, __);
-        assertEquals(2 * 3 + 4, __);
-        assertEquals(2 - 3 + 4, __);
-        assertEquals(2 + 4 / 2, __);
-        assertEquals((2 + 4) / 2, __);
+        assertEquals(1, 1);
+        assertEquals(1 + 1, 2);
+        assertEquals(2 + 3 * 4, 14);
+        assertEquals((2 + 3) * 4, 20);
+        assertEquals(2 * 3 + 4, 10);
+        assertEquals(2 - 3 + 4, 3);
+        assertEquals(2 + 4 / 2, 4);
+        assertEquals((2 + 4) / 2, 3);
     }
 
     @Test
     public void notSoSimpleOperations() {
-        assertEquals(1 / 2, __);
-        assertEquals(3 / 2, __);
-        assertEquals(1 % 2, __);
-        assertEquals(3 % 2, __);
+        assertEquals(1 / 2, 0);
+        assertEquals(3 / 2, 1);
+        assertEquals(1 % 2, 1);
+        assertEquals(3 % 2, 1);
     }
 
     @Test
     public void minusMinusVariableMinusMinus() {
         int i = 1;
-        assertEquals(--i, __);
-        assertEquals(i, __);
-        assertEquals(i--, __);
-        assertEquals(i, __);
+        assertEquals(--i, 0);
+        assertEquals(i, 0);
+        assertEquals(i--, 0);
+        assertEquals(i, -1);
     }
 
     @Test
     public void plusPlusVariablePlusPlus() {
         int i = 1;
-        assertEquals(++i, __);
-        assertEquals(i, __);
-        assertEquals(i++, __);
-        assertEquals(i, __);
+        assertEquals(++i, 2);
+        assertEquals(i, 2);
+        assertEquals(i++, 2);
+        assertEquals(i, 3);
     }
 
     @Test
     public void timesAndDivInPlace() {
         int i = 1;
         i *= 2;
-        assertEquals(i, __);
+        assertEquals(i, 2);
         i /= 2;
-        assertEquals(i, __);
+        assertEquals(i, 1);
     }
 
 }
